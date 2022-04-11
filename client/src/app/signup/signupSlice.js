e4db2a3a02020110543d0062ee4d26b7951b9b30
G Rajesh <coding3601@gmail.com>
2022-04-11 20:19:27 +0530

    changed filename to util

--- client/src/app/signup/signupSlice.js
+++ client/src/app/signup/signupSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, current } from '@reduxjs/toolkit'
-import { formDetails } from '../../Util/data'
+import { formDetails } from '../../util/data'
 
 let data = formDetails;
 if(localStorage.getItem("signupState")){
